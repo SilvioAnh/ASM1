245e7f13aa0e524d71172bf7b31be8133ac2396a
SilvioAnh <94777000+SilvioAnh@users.noreply.github.com>
2022-01-22 00:45:37 +0700

    Update App.js

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -2,7 +2,7 @@ import
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap'
-class App extends Comment {
+class App extends Component {
   render() {
     return (
       <div className="App">
