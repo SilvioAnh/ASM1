8bd8579adb535a6f633dc05eeeed72d31ef46eeb
Tuan Anh <anhsilvio@gmail.com>
2022-01-27 00:06:16 +0700

    Fourth

---
+++ src/component/StaffListComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import Media from 'reactstrap';
+import {STAFFS} from "../shared/staffs";
+
+class StaffList extends Component{
+  constructor(props) {
+    super(props);
+    this.state.names ={
+      staffs : STAFFS
+    }
+  }
+
+  render() {
+    const menu = this.state.staffs.map((names) => {
+      return (
+        <div key={names.id} className="col-12 mt-5">
+          <Media tag="li">
+            <Media body className="ml-5">
+              <Media heading>{names.name}</Media>
+            </Media>
+          </Media>
+        </div>
+
+      );
+    })
+    return(
+      <div className="container">
+        <div className="row">
+          <Media list>
+            {menu}
+          </Media>
+        </div>
+
+      </div>
+    )
+  }
+
+}
+export default StaffList;
