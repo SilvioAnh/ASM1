052fb9d482ec3d891d3f41176aa060ebf773cf2e
Tuan Anh <anhsilvio@gmail.com>
2022-02-08 23:10:01 +0700

    Sixth

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import React, { Component } from "react";
 import {Navbar, NavbarBrand} from "reactstrap";
 import StaffList from "./component/StaffListComponent";
 import './App.css';
+import { STAFFS} from "./shared/staffs";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffs : STAFFS
+    }
+  }
   render() {
     return (
       <div className="App">
         <Navbar dark color ="primary">
-          <div className="container" >
+          <div className="container">
             <NavbarBrand>
               Ứng dụng quản lý nhân sự v1.0
             </NavbarBrand>
           </div>
         </Navbar>
-        <StaffList />
+        <StaffList staffs = {this.state.staffs} />
       </div>
     );
   }
 }
-
 export default App;
--- src/component/StaffListComponent.js
+++ src/component/StaffListComponent.js
@@ -1,32 +1,57 @@
 import React, { Component } from "react";
-import {Media} from 'reactstrap';
-import {STAFFS} from "../shared/staffs";
+import {Card, CardTitle, CardBody, CardText } from 'reactstrap';
+// import {STAFFS} from "../shared/staffs";
 
 class StaffList extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      staffs : STAFFS
+      selectedStaff : null //Ban đầu chưa chọn vào bất kỳ nhân viên nào.
+    }
+  }
+  onStaffSelected(names){
+    this.setState({selectedStaff: names})
+  }
+  renderStaff(names){
+    if(names != null){
+      return(
+        <Card>
+          <CardBody>
+            <CardTitle>Họ và tên: {names.name}</CardTitle>
+            <CardText>Ngày sinh: {names.doB}</CardText>
+            <CardText>Ngày vào công ty: {names.startDate}</CardText>
+            <CardText>Phòng ban: {names.department}</CardText>
+            <CardText>Số ngày nghỉ còn lại: {names.annualLeave}</CardText>
+            <CardText>Số ngày đã làm thêm: {names.overTime}</CardText>
+          </CardBody>
+        </Card>
+      )
+    }
+    else{
+      return(
+        <div> </div>
+      );
     }
   }
   render() {
-    const menu = this.state.staffs.map((names) => {
+    const menu = this.props.staffs.map((names) => {
       return (
-        <div key={names.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media body className="ml-5">
-              <p>{names.name}</p>
-            </Media>
-          </Media>
+        <div key={names.id} className="col-12 col-md-4 m-1">
+          <Card onClick= {() => this.onStaffSelected(names) //onClick: khi click vào tên nhân viên sẽ thực hiện lệch
+          }>
+            {/*<CardImg width="100%" src={names.image} alt={names.name}/>*/}
+            <CardTitle>{names.name}</CardTitle>
+          </Card>
         </div>
       );
     })
     return(
       <div className="container">
         <div className="row">
-          <Media list>
             {menu}
-          </Media>
+        </div>
+        <div className="row">
+          {this.renderStaff(this.state.selectedStaff)}
         </div>
       </div>
     )
