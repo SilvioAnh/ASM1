5e459abdea45a50cf5b8dbcd98189898c5eab5ac
Tuan Anh <anhsilvio@gmail.com>
2022-03-05 01:37:46 +0700

    update mut button thay doi cot trong danh sach nhan vien

--- src/component/StaffListComponent.js
+++ src/component/StaffListComponent.js
@@ -7,7 +7,11 @@ class StaffList extends Component{
     super(props);
     this.state = {
       selectedStaff : null,
+      onCol:"col-6 col-lg-2 col-md-4 m-3"
     }
+  }
+  onColSelected(col){
+    this.setState({onCol: col})
   }
   onStaffSelected(names){
     this.setState({selectedStaff: names})
@@ -15,7 +19,7 @@ class StaffList extends Component{
   renderStaff(names){
     if(names != null){
       return(
-        <Card className="card1">
+        <Card>
             <CardTitle>Họ và tên: {names.name}</CardTitle>
             <CardText>Ngày sinh: {dateFormat(names.doB, "dd/mm/yyyy")}</CardText>
             <CardText>Ngày vào công ty: {dateFormat(names.startDate, "dd/mm/yyyy")}</CardText>
@@ -34,7 +38,7 @@ class StaffList extends Component{
   render() {
     const menu = this.props.staffs.map((names) => {
         return (
-          <div key={names.id} className="col-2 mt-2 mb-2">
+          <div key={names.id} className={this.state.onCol}>
             <Card onClick={() => this.onStaffSelected(names)}>
               <CardImg width="100%" src={names.image} alt={names.name}/>
               <CardTitle className="m-auto">{names.name}</CardTitle>
@@ -43,16 +47,20 @@ class StaffList extends Component{
         );
     })
     return(
-      <React.Fragment>
         <div className="container">
-        <div className="row">
-          {menu}
-        </div>
-        <div className="row">
-          {this.renderStaff(this.state.selectedStaff)}
-        </div>
+          <div className="row m-3">
+            <button className="btn btn-success mr-3"
+             onClick={()=> this.onColSelected("col-12 col-lg-3 col-md-4 m-3")}>
+              Điều chỉnh cột
+            </button>
+          </div>
+          <div className="row">
+            {menu}
+          </div>
+          <div className="row">
+            {this.renderStaff(this.state.selectedStaff)}
+          </div>
       </div>
-    </React.Fragment>
     )
   }
 }
