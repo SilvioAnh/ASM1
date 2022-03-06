aa69ac3a90679e0c239a590bb76ea65c7ca10ffe
Tuan Anh <anhsilvio@gmail.com>
2022-03-07 00:34:17 +0700

    Lab05-4

--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -1,16 +1,10 @@
-import React, {Component}from "react";
-import {CardImg, Card, CardBody,CardTitle,CardText} from "reactstrap";
+import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
-class DishDetail extends Component{
-    constructor(props) {
-        super(props);
-        this.state={
-        }
-    }
-
-    renderDish(dish) {
+    function  RenderDish({dish}) {
         return (
-            <div className="col-12 col-md-5 m-1">
+            <div>
                 <Card>
                     <CardImg width="100%" src={dish.image} value={dish.name} />
                     <CardBody>
@@ -22,10 +16,10 @@ class DishDetail extends Component{
         );
     }
 
-    RenderComments(comments) {
+    function RenderComments({comments}) {
         if (comments != null) {
             return (
-                <div className="col-12 col-md-5 m-1">
+                <div>
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
                         {comments.map((comment) => {
@@ -49,20 +43,32 @@ class DishDetail extends Component{
             return <div/>;
         }
     }
-
-    render() {
-        if (this.props.dish != null) {
+    function DishDetail(props) {
+        if (props.dish != null) {
             return (
                 <div className="container">
                     <div className="row">
-                        {this.renderDish(this.props.dish)}
-                        {this.RenderComments(this.props.dish.comments)}
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderDish dish={props.dish} />
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <RenderComments comments={props.comments} />
+                        </div>
                     </div>
                 </div>
             );
         } else {
             return <div/>;
         }
-    }
 }
 export default DishDetail;
--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from "./HomeComponent";
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
-//import DishDetail from './DishdetailComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -24,10 +24,6 @@ class Main extends Component {
         };
     }
 
-    // onDishSelect(dishId) {
-    //     this.setState({ selectedDish: dishId});
-    // }
-
     render() {
         const HomePage = () => {
             return(
@@ -38,6 +34,12 @@ class Main extends Component {
                 />
             );
         }
+        const DishWithId = ({match}) => {
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+        };
         return (
             <div>
                 <Header />
@@ -45,10 +47,9 @@ class Main extends Component {
                     <Route path="/home" component={HomePage}/>
                     <Route exact path="/menu" component={() =><Menu dishes={this.state.dishes}/>}/>
                     <Route exact path='/contactus' component={Contact} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to="/home"/>
                 </Switch>
-                {/*<Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />*/}
-                {/*<DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />*/}
                 <Footer />
             </div>
         );
