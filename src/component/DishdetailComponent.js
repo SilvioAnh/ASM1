edd125cd67a32f556baf0b280446f6dd2f10a3e8
Tuan Anh <anhsilvio@gmail.com>
2022-03-06 17:44:01 +0700

    Lab04-1

--- src/component/DishdetailComponent.js
+++ src/component/DishdetailComponent.js
@@ -33,7 +33,11 @@ class DishDetail extends Component{
                                 <li key={comment.id}>
                                     <p>{comment.comment}</p>
                                     <p>
-                                        -- {comment.author}, {comment.date}
+                                        -- {comment.author},
+                                        {new Intl.DateTimeFormat('en-US',
+                                            { year: 'numeric', month: 'short', day: '2-digit'}).format(
+                                                new Date(Date.parse(comment.date)))
+                                        }
                                     </p>
                                 </li>
                             );
