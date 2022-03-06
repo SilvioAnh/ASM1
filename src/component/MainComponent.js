7666f7c200c1d5dd9ed16e9bf78edf41fb0b98a4
Tuan Anh <anhsilvio@gmail.com>
2022-03-07 01:44:37 +0700

    Exercise 2

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Home from "./HomeComponent";
+import About from "./AboutComponent";
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
@@ -46,6 +47,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage}/>
                     <Route exact path="/menu" component={() =><Menu dishes={this.state.dishes}/>}/>
+                    <Route exact path="/aboutus" component={() => <About leaders = {this.state.leaders}/>}/>
                     <Route exact path='/contactus' component={Contact} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to="/home"/>
