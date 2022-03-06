42fad2a477b80b361cf345c0aca95e7398ac5ba4
Tuan Anh <anhsilvio@gmail.com>
2022-03-06 22:32:26 +0700

    Lab05-2

--- src/component/text.js
+++ src/component/text.js
@@ -1,52 +1,58 @@
-import React, {Component} from "react";
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, Nav, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import {NavLink} from "react-router-dom";
 
 class Header extends Component {
     constructor(props) {
         super(props);
         this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
+        this.state={
             isNavOpen: false
         };
     }
-    toggleNav() {
+    toggleNav(){
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
     render() {
         return(
+            // Khi sử dụng <React.Fragment> sẽ không tạo ra node mới khi sử dụng, các React element sẽ được thêm trực tiếp vào DOM
             <React.Fragment>
                 <Navbar dark expand="md">
                     <div className="container">
-                        <NavbarToggler onClick={this.toggleNav} />
+                        <NavbarToggler onClick={this.toggleNav}/>
                         <NavbarBrand className="mr-auto" href="/">
-                            <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
+                            <img src="assets/images/logo.png" height="30" width="41" alt="Ristorante Con Fusion"/>
                         </NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
-                            <Nav navbar>
+                            <Nav Navbar>
                                 <NavItem>
                                     <NavLink className="nav-link" to="/home">
                                         <span className="fa fa-home fa-lg"/>Home
                                     </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/aboutus'>
-                                        <span className="fa fa-info fa-lg"/> About Us</NavLink>
+                                    <NavLink className="nav-link" to="/aboutus">
+                                        <span className="fa fa-info fa-lg"/>About Us
+                                    </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link"  to='/menu'>
-                                        <span className="fa fa-list fa-lg"/> Menu</NavLink>
+                                    <NavLink className="nav-link" to="/menu">
+                                        <span className="fa fa-list fa-lg"/>Menu
+                                    </NavLink>
                                 </NavItem>
                                 <NavItem>
-                                    <NavLink className="nav-link" to='/contactus'>
-                                        <span className="fa fa-address-card fa-lg"/> Contact Us</NavLink>
+                                    <NavLink className="nav-link" to="/contactus">
+                                        <span className="fa fa-address-card fa-lg"/>Contact Us
+                                    </NavLink>
                                 </NavItem>
                             </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
+
+                {/* Jumbotron cho phép hiển thị một số thông tin trên phân header*/}
                 <Jumbotron>
                     <div className="container">
                         <div className="row row-header">
