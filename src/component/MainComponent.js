f2efe6da8c4c0b04f1679ae14ce84bc1f06b5ae2
Tuan Anh <anhsilvio@gmail.com>
2022-03-11 00:28:40 +0700

    Lab06-1

--- src/component/MainComponent.js
+++ src/component/MainComponent.js
@@ -15,6 +15,7 @@ import {Switch, Route, Redirect} from "react-router-dom";
 
 class Main extends Component {
 
+
     constructor(props) {
         super(props);
         this.state = {
@@ -35,7 +36,7 @@ class Main extends Component {
                 />
             );
         }
-        const DishWithId = ({match}) => {
+        const DishWithId = ({match}) => { 
             return(
                 <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
                             comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
@@ -47,9 +48,9 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage}/>
                     <Route exact path="/menu" component={() =><Menu dishes={this.state.dishes}/>}/>
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path="/aboutus" component={() => <About leaders = {this.state.leaders}/>}/>
                     <Route exact path='/contactus' component={Contact} />} />
-                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Redirect to="/home"/>
                 </Switch>
                 <Footer />
